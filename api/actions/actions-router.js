bc3a4dbd60f1c06dd19ea5de1d4fecc3990ffe86
Jesus Navarrete <jesusnavarrete@Jesuss-MacBook-Pro.local>
2021-10-29 06:21:21 -0700

    26 tests passed

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,26 +1,84 @@
 // Write your "actions" router here!
-const express = require('express')
-const actions = require('./actions-model')
-const router = express.Router()
+const express = require("express");
+const Actions = require("./actions-model");
+const router = express.Router();
 
+router.get("/", async (req, res, next) => {
+  try {
+    const action = await Actions.get();
+    if (!action) {
+      res.json([]);
+    } else {
+      res.json(action);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.get('',() => {
-
-})
-
-router.get('',() => {
-    
-})
-router.post('',() => {
-    
-})
-router.put('',() => {
-    
-})
-router.delete('',() => {
-    
-})
-
-
+router.get("/:id", async (req, res, next) => {
+  try {
+    const action = await Actions.get(req.params.id);
+    if (!action) {
+      res
+        .status(404)
+        .json({ message: "The project with the specific ID does not exist" });
+    } else {
+      res.json(action);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+router.post("/", async (req, res, next) => {
+  const { project_id, description, notes } = req.body;
+  if (!project_id || !description || !notes) {
+    res
+      .status(400)
+      .json({
+        message: "Please provide the project ID, notes,  and description",
+      });
+  } else {
+    Actions.insert(req.body)
+      .then((action) => {
+        res.status(201).json(action);
+      })
+      .catch(next);
+  }
+});
+router.put("/:id", (req, res, next) => {
+  const { project_id, description, notes } = req.body;
+  if (!project_id || !description ) {
+    res.status(400).json({ message: "Please provide name and description" });
+  } else if (!req.params.id) {
+    res
+      .status(404)
+      .json({ message: "The post with teh specified ID does not exist " });
+  } else {
+    Actions.update(req.params.id, req.body)
+      .then(() => {
+        return Actions.get(req.params.id);
+      })
+      .then((action) => {
+        res.json(action);
+      })
+      .catch(next);
+  }
+});
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const action = await Actions.get(req.params.id);
+    if (!action) {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist" });
+    } else {
+      await Actions.remove(req.params.id);
+      res.json(action);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
